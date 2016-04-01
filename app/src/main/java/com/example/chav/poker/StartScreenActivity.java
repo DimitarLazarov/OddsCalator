528140212124339cf1c04e100c5ff7396d1e6a7d
Mitakaa <dlazarov94@gmail.com>
2016-04-01 17:55:23 +0300

    onClickListener added i start activiti buttons

--- app/src/main/java/com/example/chav/poker/StartScreenActivity.java
+++ app/src/main/java/com/example/chav/poker/StartScreenActivity.java
@@ -1,13 +1,30 @@
 package com.example.chav.poker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreenActivity extends AppCompatActivity {
+
+    private Button oddsButton;
+    private Button quizButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        oddsButton = (Button) findViewById(R.id.odds_button);
+        oddsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        quizButton = (Button) findViewById(R.id.quiz_button);
     }
 }
