a36f3e20c7adfb0154fda79f32ed71608ceae9f0
Chavdar Nedialkov <chavdar.nediakov@gmail.com>
2016-04-05 18:36:15 +0300

    Add Cram Card Activity

---
+++ app/src/main/java/com/example/chav/poker/controller/CramCardCreate.java
@@ -0,0 +1,54 @@
+package com.example.chav.poker.controller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.chav.poker.R;
+
+public class CramCardCreate extends AppCompatActivity {
+
+    private EditText mFront;
+    private EditText mBack;
+    private Button mCancel;
+    private Button mConfirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cram_card_create);
+
+        mFront = (EditText) findViewById(R.id.create_cram_card_front);
+        mBack = (EditText) findViewById(R.id.create_cram_card_back);
+        mCancel = (Button) findViewById(R.id.create_cram_card_confirm);
+        mConfirm = (Button) findViewById(R.id.create_cram_card_cancel);
+        mConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!mFront.getText().toString().isEmpty() && !mBack.getText().toString().trim().isEmpty()){
+                    finishWithResult();
+                }
+            }
+        });
+
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void finishWithResult(){
+        Bundle conData = new Bundle();
+        conData.putString("front_result", mFront.getText().toString());
+        conData.putString("back_result", mBack.getText().toString());
+        Intent intent = new Intent();
+        intent.putExtras(conData);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
