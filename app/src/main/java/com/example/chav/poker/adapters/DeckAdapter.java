861f13dde991fdf1773fa4ebab81fc4c6f4810c0
Mitakaa <dlazarov94@gmail.com>
2016-04-05 15:34:22 +0300

    cram decks added recycler view

---
+++ app/src/main/java/com/example/chav/poker/adapters/DeckAdapter.java
@@ -0,0 +1,78 @@
+package com.example.chav.poker.adapters;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.chav.poker.R;
+
+import java.util.List;
+
+import model.CramDeck;
+
+/**
+ * Created by Mitakaa on 05-Apr-16.
+ */
+public class DeckAdapter extends RecyclerView.Adapter<DeckAdapter.DeckViewHolder>{
+
+    private List<CramDeck> decks;
+
+    public DeckAdapter(List<CramDeck> decks) {
+        this.decks = decks;
+    }
+
+
+    public static class DeckViewHolder extends RecyclerView.ViewHolder {
+        TextView mDeckTitle;
+
+        public DeckViewHolder(View itemView) {
+            super(itemView);
+            mDeckTitle = (TextView) itemView.findViewById(R.id.view_holder_deck_text_view);
+        }
+    }
+
+    public static class VerticalSpaceItemDecoration extends RecyclerView.ItemDecoration {
+        private final int mVerticalSpaceHeight;
+
+        public VerticalSpaceItemDecoration(int mVerticalSpaceHeight) {
+            this.mVerticalSpaceHeight = mVerticalSpaceHeight;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                                   RecyclerView.State state) {
+            outRect.bottom = mVerticalSpaceHeight;
+        }
+
+    }
+
+    @Override
+    public DeckViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyvler_view_holder_deck, parent, false);
+        DeckViewHolder dvh = new DeckViewHolder(v);
+        return dvh;
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(DeckViewHolder holder, int position) {
+        holder.mDeckTitle.setText(decks.get(position).getTitle());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return decks.size();
+    }
+
+
+}
--- app/src/main/java/com/example/chav/poker/controller/CramCardsUserActivity.java
+++ app/src/main/java/com/example/chav/poker/controller/CramCardsUserActivity.java
@@ -1,15 +1,72 @@
 package com.example.chav.poker.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.chav.poker.R;
+import com.example.chav.poker.adapters.DeckAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.CramDeck;
 
 public class CramCardsUserActivity extends AppCompatActivity {
+
+    private Button mCreateNewDeck;
+    private Button mMyDecks;
+    private RecyclerView mViewOfDecks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cram_cards_user);
+
+        mViewOfDecks = (RecyclerView) findViewById(R.id.my_cram_cards_recycler);
+        mCreateNewDeck = (Button)findViewById(R.id.user_cram_add_deck);
+        mCreateNewDeck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startActivityNewDeck = new Intent();
+            }
+        });
+        mMyDecks = (Button)findViewById(R.id.user_cram_sets);
+        mMyDecks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMyDecks.isActivated()) {
+                    mMyDecks.setActivated(false);
+                    mViewOfDecks.setVisibility(View.GONE);
+                }
+                else {
+                    mMyDecks.setActivated(true);
+                    mViewOfDecks.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        CramDeck deck1 = new CramDeck("Title1");
+        CramDeck deck2 = new CramDeck("Title2");
+        CramDeck deck3 = new CramDeck("Title3");
+        CramDeck deck4 = new CramDeck("Title4");
+        List<CramDeck> deckCards = new ArrayList<>();
+        deckCards.add(deck1);
+        deckCards.add(deck2);
+        deckCards.add(deck3);
+        deckCards.add(deck4);
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mViewOfDecks.setLayoutManager(layoutManager);
+        DeckAdapter adapter = new DeckAdapter(deckCards);
+        mViewOfDecks.addItemDecoration(new DeckAdapter.VerticalSpaceItemDecoration(3));
+        mViewOfDecks.setAdapter(adapter);
+
+
     }
 }
