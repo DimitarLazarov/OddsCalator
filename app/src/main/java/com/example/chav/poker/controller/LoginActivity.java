94410660514e67a859954528e7e5c760518133b2
Chavdar Nedialkov <chavdar.nediakov@gmail.com>
2016-04-06 21:32:07 +0300

    Login and Register Change focus and enter starts the next activity

--- app/src/main/java/com/example/chav/poker/controller/LoginActivity.java
+++ app/src/main/java/com/example/chav/poker/controller/LoginActivity.java
@@ -3,9 +3,12 @@ package com.example.chav.poker.controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.chav.poker.R;
 import com.example.chav.poker.managers.UsersManager;
@@ -30,6 +33,42 @@ public class LoginActivity extends AppCompatActivity {
         mSubmit = (Button) findViewById(R.id.login_submit_button);
         mCancel = (Button) findViewById(R.id.login_cancel_button);
 
+        mUsername.setNextFocusDownId(R.id.login_password);
+        mUsername.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on Enter key press
+                    mUsername.clearFocus();
+                    mPassword.requestFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    if (UsersManager.getInstance(v.getContext()).login(mUsername.getText().toString(), mPassword.getText().toString())) {
+                        User user = UsersManager.getInstance(v.getContext()).getUser(mUsername.getText().toString());
+                        UsersManager.getInstance(v.getContext()).setCurrentUser(user);
+                        SavedSharedPreferences.setUserName(v.getContext(), user.getUsername());
+                        Intent i = new Intent(v.getContext(), QuizSelectionActivity.class);
+                        startActivity(i);
+                        return true;
+                    } else {
+                        Toast.makeText(v.getContext(), "Invalid username or password", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                }
+                return false;
+            }
+        });
+
         mCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,6 +86,8 @@ public class LoginActivity extends AppCompatActivity {
                     Intent i = new Intent(v.getContext(), QuizSelectionActivity.class);
 
                     startActivity(i);
+                } else {
+                    Toast.makeText(v.getContext(), "Invalid username or password", Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- app/src/main/java/com/example/chav/poker/controller/RegisterActivity.java
+++ app/src/main/java/com/example/chav/poker/controller/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.chav.poker.controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,27 +35,69 @@ public class RegisterActivity extends AppCompatActivity {
         mSubmit = (Button) findViewById(R.id.register_submit_button);
         mCancel = (Button) findViewById(R.id.register_cancel_button);
 
+        mUsername.setNextFocusDownId(R.id.register_email);
+        mUsername.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on Enter key press
+                    mUsername.clearFocus();
+                    mEmail.requestFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mEmail.setNextFocusDownId(R.id.register_password);
+        mPass.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on Enter key press
+                    mEmail.clearFocus();
+                    mPass.requestFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mPass.setNextFocusDownId(R.id.register_confirm_password);
+        mPass.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on Enter key press
+                    mPass.clearFocus();
+                    mConfirmPass.requestFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mConfirmPass.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                        && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on Enter key press
+                    startMainActivity();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
         mSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = mUsername.getText().toString();
-                String email = mEmail.getText().toString();
-                String pass = mPass.getText().toString();
-                String confirmPass = mConfirmPass.getText().toString();
-
-                if (isLegalUsername(username)
-                        && isLegalEMail(email)
-                        && isLegalPassword(pass, confirmPass)) {
-
-                    User user = new User(username, pass, email);
-                    UsersManager.getInstance(v.getContext()).registerUser(user);
-                    SavedSharedPreferences.setUserName(v.getContext(), user.getUsername());
-                    Intent i = new Intent(v.getContext(), QuizSelectionActivity.class);
-                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(i);
-
-                }
-
+                startMainActivity();
             }
         });
 
@@ -129,5 +172,25 @@ public class RegisterActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    private void startMainActivity(){
+        String username = mUsername.getText().toString();
+        String email = mEmail.getText().toString();
+        String pass = mPass.getText().toString();
+        String confirmPass = mConfirmPass.getText().toString();
+
+        if (isLegalUsername(username)
+                && isLegalEMail(email)
+                && isLegalPassword(pass, confirmPass)) {
+
+            User user = new User(username, pass, email);
+            UsersManager.getInstance(this).registerUser(user);
+            SavedSharedPreferences.setUserName(this, user.getUsername());
+            Intent i = new Intent(this, QuizSelectionActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+
+        }
     }
 }
