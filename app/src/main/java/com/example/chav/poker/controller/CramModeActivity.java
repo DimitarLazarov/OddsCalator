ea3e1aa481672a73e5070694dfc8aa3fd10228b4
Mitakaa <dlazarov94@gmail.com>
2016-04-06 09:07:52 +0300

    cram mode activity flip cards

--- app/src/main/java/com/example/chav/poker/controller/CramModeActivity.java
+++ app/src/main/java/com/example/chav/poker/controller/CramModeActivity.java
@@ -13,6 +13,7 @@ import com.example.chav.poker.R;
 public class CramModeActivity extends AppCompatActivity {
 
     private FrameLayout mCardLayout;
+    private boolean mPressed = false;
 
 
     @Override
@@ -40,31 +41,59 @@ public class CramModeActivity extends AppCompatActivity {
         // Create and commit a new fragment transaction that adds the fragment for
         // the back of the card, uses custom animations, and is part of the fragment
         // manager's back stack.
+        if(!mPressed) {
+            mPressed = true;
+            getFragmentManager()
+                    .beginTransaction()
 
-        Log.e("greshka","sss");
-        getFragmentManager()
-                .beginTransaction()
+                            // Replace the default fragment animations with animator resources
+                            // representing rotations when switching to the back of the card, as
+                            // well as animator resources representing rotations when flipping
+                            // back to the front (e.g. when the system Back button is pressed).
+                    .setCustomAnimations(
+                            R.anim.fragment_card_flip_rigth_in,
+                            R.anim.fragment_card_flip_rigth_out,
+                            R.anim.fragment_card_flip_left_in,
+                            R.anim.fragment_card_flip_left_out)
 
-                        // Replace the default fragment animations with animator resources
-                        // representing rotations when switching to the back of the card, as
-                        // well as animator resources representing rotations when flipping
-                        // back to the front (e.g. when the system Back button is pressed).
-                .setCustomAnimations(
-                        R.anim.fragment_card_flip_rigth_in,
-                        R.anim.fragment_card_flip_rigth_out,
-                        R.anim.fragment_card_flip_left_in,
-                        R.anim.fragment_card_flip_left_out)
+                            // Replace any fragments currently in the container view with a
+                            // fragment representing the next page (indicated by the
+                            // just-incremented currentPage variable).
+                    .replace(R.id.cram_mode_frame_layout, new CramCardFrontEndFragment())
 
-                        // Replace any fragments currently in the container view with a
-                        // fragment representing the next page (indicated by the
-                        // just-incremented currentPage variable).
-                .replace(R.id.cram_mode_frame_layout, new CardsAndOddsFragment())
+                            // Add this transaction to the back stack, allowing users to press
+                            // Back to get to the front of the card.
+                    .addToBackStack(null)
 
-                        // Add this transaction to the back stack, allowing users to press
-                        // Back to get to the front of the card.
-                .addToBackStack(null)
+                            // Commit the transaction.
+                    .commit();
+        }
+        else {
+            mPressed = false;
+            getFragmentManager()
+                    .beginTransaction()
 
-                        // Commit the transaction.
-                .commit();
+                            // Replace the default fragment animations with animator resources
+                            // representing rotations when switching to the back of the card, as
+                            // well as animator resources representing rotations when flipping
+                            // back to the front (e.g. when the system Back button is pressed).
+                    .setCustomAnimations(
+                            R.anim.fragment_card_flip_left_in,
+                            R.anim.fragment_card_flip_left_out,
+                            R.anim.fragment_card_flip_rigth_in,
+                            R.anim.fragment_card_flip_rigth_out)
+
+                            // Replace any fragments currently in the container view with a
+                            // fragment representing the next page (indicated by the
+                            // just-incremented currentPage variable).
+                    .replace(R.id.cram_mode_frame_layout, new CramCardFrontEndFragment())
+
+                            // Add this transaction to the back stack, allowing users to press
+                            // Back to get to the front of the card.
+                    .addToBackStack(null)
+
+                            // Commit the transaction.
+                    .commit();
+        }
     }
 }
