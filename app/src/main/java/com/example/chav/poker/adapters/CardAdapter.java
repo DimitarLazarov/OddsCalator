e623bd1157d6a7deabdbd557afb2d55593e7a427
Mitakaa <dlazarov94@gmail.com>
2016-04-08 15:06:37 +0300

    cram cards view changes

--- app/src/main/java/com/example/chav/poker/adapters/CardAdapter.java
+++ app/src/main/java/com/example/chav/poker/adapters/CardAdapter.java
@@ -28,18 +28,27 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
 
 
     public static class CardViewHolder extends RecyclerView.ViewHolder {
-        TextView mQuestionCard;
-        View mSeparator;
-        TextView mAnswerCard;
+        private boolean mShowOrHide = false;
+        private TextView mQuestionCard;
+        private View mSeparator;
+        private TextView mAnswerCard;
+        private TextView mAnswerHelper;
+        private TextView mQuestionHelper;
 
         public CardViewHolder(View itemView) {
             super(itemView);
             Typeface myTypeface = Typeface.createFromAsset(itemView.getContext().getAssets(), "HelveticaRoman.ttf");
             mQuestionCard = (TextView) itemView.findViewById(R.id.view_holder_card_question_view);
             mQuestionCard.setTypeface(myTypeface);
+            mAnswerHelper = (TextView) itemView.findViewById(R.id.view_holder_cram_answer_helper);
+            mAnswerHelper.setVisibility(View.GONE);
+            mQuestionHelper = (TextView) itemView.findViewById(R.id.view_holder_cram_question_helper);
+            mQuestionHelper.setVisibility(View.GONE);
             mAnswerCard = (TextView) itemView.findViewById(R.id.view_holder_card_answer_view);
             mAnswerCard.setTypeface(myTypeface);
+            mAnswerCard.setVisibility(View.GONE);
             mSeparator = (View) itemView.findViewById(R.id.view_separator);
+            mSeparator.setVisibility(View.GONE);
         }
     }
 
@@ -65,9 +74,27 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
     }
 
     @Override
-    public void onBindViewHolder(CardViewHolder holder, int position) {
+    public void onBindViewHolder(final CardViewHolder holder, int position) {
         holder.mQuestionCard.setText(cards.get(position).getQuestion());
         holder.mAnswerCard.setText(cards.get(position).getAnswer());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.mAnswerCard.getVisibility() == View.GONE) {
+                    holder.mAnswerHelper.setVisibility(View.VISIBLE);
+                    holder.mQuestionHelper.setVisibility(View.VISIBLE);
+                    holder.mAnswerCard.setVisibility(View.VISIBLE);
+                    holder.mSeparator.setVisibility(View.VISIBLE);
+                }
+                else {
+                    holder.mAnswerHelper.setVisibility(View.GONE);
+                    holder.mQuestionHelper.setVisibility(View.GONE);
+                    holder.mAnswerCard.setVisibility(View.GONE);
+                    holder.mSeparator.setVisibility(View.GONE);
+                }
+            }
+        });
 
     }
 
